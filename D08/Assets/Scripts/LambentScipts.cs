b066bea39aff24abe142a2175e6eafad5f7ff3bd
Marion DEBELLEY <mdebelle@e2r11p9.42.fr>
2015-06-25 22:21:03 +0200

    bonne nuit

---
+++ D08/Assets/Scripts/LambentScipts.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LambentScipts : MonoBehaviour {
+
+		
+	Animator				animator;
+
+	// Use this for initialization
+	void Start () {
+	
+		animator = GetComponent<Animator>();
+		animator.SetBool("idle", false);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
